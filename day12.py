342ed3872dbb7dbdea46547e4c5256f814a73bf5
Cameron Spickert <cspickert@google.com>
2020-01-10 11:38:34 -0500

    Simplify day 12 part 1

--- day12.py
+++ day12.py
@@ -1,26 +1,16 @@
-import itertools
 import numpy as np
 
-def step(pos, vel):
-    tmp = pos.reshape(len(pos), 1)
-    vel += np.where(pos == tmp, 0, np.where(pos < tmp, -1, 1)).sum(1)
-    pos += vel
+def step(data):
+    for pos, vel in data.T:
+        tmp = pos.reshape(len(pos), 1)
+        vel += np.where(pos == tmp, 0, np.where(pos < tmp, -1, 1)).sum(1)
+        pos += vel
 
 def part1(data):
-    x = np.array([moon[0] for moon in data])
-    y = np.array([moon[1] for moon in data])
-    z = np.array([moon[2] for moon in data])
-    xv = np.zeros(4, dtype=int)
-    yv = np.zeros(4, dtype=int)
-    zv = np.zeros(4, dtype=int)
     for _ in range(1000):
-        step(x, xv)
-        step(y, yv)
-        step(z, zv)
+        step(data)
     total_energy = 0
-    for i in range(len(data)):
-        pos = np.array([x[i], y[i], z[i]])
-        vel = np.array([xv[i], yv[i], zv[i]])
+    for pos, vel in data:
         pot = np.abs(pos).sum()
         kin = np.abs(vel).sum()
         total_energy += pot * kin
@@ -29,13 +19,18 @@ def part1(data):
 def part2(data):
     pass
 
-if __name__ == '__main__':
+def load_data():
     from input import day12
     from parse import parse
     data = []
     for line in day12.splitlines():
-        r = parse('<x={x:d}, y={y:d}, z={z:d}>', line)
-        if r:
-            data.append((r['x'], r['y'], r['z']))
+        result = parse('<x={:d}, y={:d}, z={:d}>', line)
+        if result:
+            moon = np.array([list(result), [0] * 3])
+            data.append(moon)
+    return np.array(data)
+
+if __name__ == '__main__':
+    data = load_data()
     part1(data)
     # part2(data)
