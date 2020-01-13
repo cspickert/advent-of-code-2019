40e5ac79713efbb04a0c2c13a2906fe0e6ac41e7
Cameron Spickert <cspickert@google.com>
2020-01-13 17:29:23 -0500

    Add solution for day 14 part 2

--- day14.py
+++ day14.py
@@ -1,39 +1,57 @@
+import math
+
 from collections import defaultdict
 
-def produce(requested_amount, chemical, reactions, materials=defaultdict(int)):
-    try:
-        chem_output = next(key for key in reactions if key[1] == chemical)
-    except StopIteration:
-        materials[chemical] += requested_amount
-        return requested_amount
-    chem_inputs = reactions[chem_output]
-    total_ore_used = 0
-    for input_amount, input_chemical in chem_inputs:
-        while materials[input_chemical] < input_amount:
-            total_ore_used += produce(
-                input_amount, input_chemical, reactions, materials)
-        materials[input_chemical] -= input_amount
-    output_amount, output_chemical = chem_output
-    materials[output_chemical] += output_amount
-    return total_ore_used
+def produce_fuel(fuel_amount, reactions):
+    total_ore = 0
+    materials = defaultdict(int)
+    stack = [(fuel_amount, 'FUEL')]
+    while stack:
+        requested_amount, chemical = stack.pop()
+        if chemical not in reactions:
+            total_ore += requested_amount
+        else:
+            requested_amount -= materials[chemical]
+            output_amount, inputs = reactions[chemical]
+            multiplier = math.ceil(requested_amount / output_amount)
+            for input_amount, input_chemical in inputs:
+                stack.append((input_amount * multiplier, input_chemical))
+            materials[chemical] = output_amount * multiplier - requested_amount
+    return total_ore
 
 def part1(reactions):
-    total_ore = produce(1, 'FUEL', reactions)
+    total_ore = produce_fuel(1, reactions)
     print(total_ore)
 
-if __name__ == '__main__':
-    import parse
-    from input import day14
+def part2(reactions):
+    max_ore = 1000000000000
+    fuel_amount = 1
+    while produce_fuel(fuel_amount, reactions) < max_ore:
+        fuel_amount *= 2
+    upper, lower = fuel_amount, fuel_amount / 2
+    while upper - lower > 0.001:
+        fuel_amount = (lower + upper) / 2
+        result = produce_fuel(fuel_amount, reactions)
+        if result == max_ore: break
+        elif result < max_ore: lower = fuel_amount
+        elif result > max_ore: upper = fuel_amount
+    print(math.floor(fuel_amount))
 
+def load_reactions(string):
+    import parse
     reactions = {}
     pattern = parse.compile('{:d} {:w}')
-
-    for line in day14.splitlines():
+    for line in string.splitlines():
         inputs_str, output_str = line.split(' => ')
         inputs = []
         for result in pattern.findall(inputs_str):
             inputs.append(tuple(result))
-        output = tuple(pattern.search(output_str))
-        reactions[output] = inputs
-    part1(reactions)
+        output_amount, chemical = tuple(pattern.search(output_str))
+        reactions[chemical] = (output_amount, inputs)
+    return reactions
 
+if __name__ == '__main__':
+    from input import day14
+    reactions = load_reactions(day14)
+    # part1(reactions)
+    part2(reactions)
